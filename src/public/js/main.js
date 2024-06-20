f4d4ac36bccbae7c1bd2b2a338a3c781e1d5ad5c
ChristianP5 <christiankps111@gmail.com>
2024-06-21 03:09:00 +0700

    updated html document

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,11 +1,57 @@
-const toggleBtn = document.querySelector('.toggle_btn');
-const toggleBtnIcon = document.querySelector('.toggle_btn i');
-const dropDownMenu = document.querySelector('.dropdown_menu');
+const toggleBtn = document.querySelector('.toggle_btn')
+    const toggleBtnIcon = document.querySelector('.toggle_btn i')
+    const dropDownMenu = document.querySelector('.dropdown_menu')
 
-toggleBtn.onclick = function () {
-  dropDownMenu.classList.toggle('open');
-  const isOpen = dropDownMenu.classList.contains('open');
-  toggleBtnIcon.classList = isOpen
-    ? 'fa-solid fa-xmark'
-    : 'fa-solid fa-bars';
-};
+    toggleBtn.onclick = function () {
+      dropDownMenu.classList.toggle('open')
+      const isOpen = dropDownMenu.classList.contains('open')
+      toggleBtnIcon.classList = isOpen
+        ? 'fa-solid fa-xmark'
+        : 'fa-solid fa-bars';
+    }
+
+
+  //   document.addEventListener("DOMContentLoaded", function() {
+  //     const fadeInElements = document.querySelectorAll('.fade-in');
+  
+  //     function checkScroll() {
+  //         fadeInElements.forEach(element => {
+  //             if (isElementInViewport(element)) {
+  //                 element.classList.add('active');
+  //             }
+  //         });
+  //     }
+  
+  //     function isElementInViewport(el) {
+  //         const rect = el.getBoundingClientRect();
+  //         return (
+  //             rect.top >= 0 &&
+  //             rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+  //         );
+  //     }
+  
+  //     checkScroll();
+  //     window.addEventListener('scroll', checkScroll);
+  // });
+
+  
+  // text fade in transition on scroll
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const elements = document.querySelectorAll('.splash, .about, .instructions, .members, .howto');
+  
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('fade-in');
+            } else {
+                entry.target.classList.remove('fade-in');
+            }
+        });
+    }, { threshold: 0.06 });
+  
+    elements.forEach(element => {
+        observer.observe(element);
+    });
+  });
+  
